2a12b9c5732c39cfe0103b68f621f40e2d2a4789
desewenkdk <desewenkdk@o365.sogang.ac.kr>
2021-03-31 22:37:01 +0900

    Hello World2

--- main.py
+++ main.py
@@ -4,13 +4,12 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
+def print_hi(name, text):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hello, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
+    print(f'Hello, {text}')
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('WORLD')
-
+    print_hi('WORLD', 'WORLD2')
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
